f5b44f280070827bee04b2ef31ef851769269ca7
Jens Lechtenboerger <jens.lechtenboerger@fsfe.org>
2017-09-24 16:00:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This project demonstrates how to use
+[emacs-reveal](/oer/emacs-reveal) to generate
+[reveal.js presentations](http://lab.hakim.se/reveal-js/)
+(HTML slides embedding audio)
+from text files in [Org Mode](http://orgmode.org/).
+
+Head over to the
+[Howto Presentation](https://oer.gitlab.io/emacs-reveal-howto/howto.html),
+which was generated from [this Org file](howto.org).
---
+++ elisp/publish.el
@@ -0,0 +1,86 @@
+;;; publish.el --- Publish reveal.js presentation from Org files on Gitlab Pages
+;; -*- Mode: Emacs-Lisp -*-
+;; -*- coding: utf-8 -*-
+
+;; Copyright (C) 2017 Jens Lechtenbörger
+
+;;; License: GPLv3
+
+;;; Commentary:
+;; Inspired by publish.el by Rasmus:
+;; https://gitlab.com/pages/org-mode/blob/master/publish.el
+
+
+;;; Code:
+(package-initialize)
+(require 'org)
+(require 'ox-publish)
+
+(setq org-export-with-smart-quotes t
+      org-confirm-babel-evaluate nil)
+
+(add-to-list 'load-path
+	     (expand-file-name
+	      "../emacs-reveal/" (file-name-directory load-file-name)))
+(require 'reveal-config)
+(setq org-reveal-root "./reveal.js"
+      org-reveal-title-slide "title-slide.html")
+
+(setq org-publish-project-alist
+      (list
+       (list "howto"
+	     :base-directory "."
+	     :base-extension "org"
+	     :exclude "config\\|license-template"
+	     :publishing-function 'org-reveal-publish-to-reveal
+	     :publishing-directory "./public")
+       (list "images"
+	     :base-directory "img"
+	     :base-extension (regexp-opt '("png" "jpg" "ico" "svg" "gif"))
+	     :publishing-directory "./public/img"
+	     :publishing-function 'org-publish-attachment)
+       (list "audios"
+	     :base-directory "audio"
+	     :base-extension (regexp-opt '("ogg" "mp3"))
+	     :publishing-directory "./public/audio"
+	     :publishing-function 'org-publish-attachment)
+       (list "reveal-static"
+	     :base-directory "emacs-reveal/reveal.js"
+	     :exclude "\\.git"
+	     :base-extension 'any
+	     :publishing-directory "./public/reveal.js"
+	     :publishing-function 'org-publish-attachment
+	     :recursive t)
+       (list "reveal-theme"
+	     :base-directory "css"
+	     :base-extension 'any
+	     :publishing-directory "./public/reveal.js/css/theme"
+	     :publishing-function 'org-publish-attachment)
+       (list "reveal-toc-plugin"
+	     :base-directory "emacs-reveal/Reveal.js-TOC-Progress/plugin"
+	     :base-extension 'any
+	     :publishing-directory "./public/reveal.js/plugin"
+	     :publishing-function 'org-publish-attachment
+	     :recursive t)
+       (list "reveal.js-plugins-anything"
+	     :base-directory "emacs-reveal/reveal.js-plugins/anything"
+	     :base-extension 'any
+	     :publishing-directory "./public/reveal.js/plugin/anything"
+	     :publishing-function 'org-publish-attachment
+	     :recursive t)
+       (list "reveal.js-plugins-audio-slideshow"
+	     :base-directory "emacs-reveal/reveal.js-plugins/audio-slideshow"
+	     :base-extension 'any
+	     :publishing-directory "./public/reveal.js/plugin/audio-slideshow"
+	     :publishing-function 'org-publish-attachment
+	     :recursive t)
+       (list "reveal.js-jump-plugin"
+	     :base-directory "emacs-reveal/reveal.js-jump-plugin/jump"
+	     :base-extension 'any
+	     :publishing-directory "./public/reveal.js/plugin/jump"
+	     :publishing-function 'org-publish-attachment
+	     :recursive t)
+       (list "site" :components '("howto"))))
+
+(provide 'publish)
+;;; publish.el ends here
